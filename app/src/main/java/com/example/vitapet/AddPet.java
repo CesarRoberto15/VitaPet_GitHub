9db341253e75f49df7b678f577e07b444ca1cfd7
unknown <gelbermartinezpastor@gmail.com>
2019-12-05 19:02:48 -0500

    Añadiendo Usuario y AddPet

--- app/src/main/java/com/example/vitapet/AddPet.java
+++ app/src/main/java/com/example/vitapet/AddPet.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class User extends AppCompatActivity {
+public class AddPet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
-    }
+        setContentView(R.layout.activity_add_pet);
 
-    public void addPet(View view){
-        Intent principalIntent= new Intent(this,AddPet.class);
+    }
+    public void addPet2(View view){
+        Intent principalIntent= new Intent(this,User.class);
         startActivity(principalIntent);
     }
 }
