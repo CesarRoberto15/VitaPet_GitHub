6e4dbe3a33c66120a9f41c60581f9f7e706a795f
unknown <gelbermartinezpastor@gmail.com>
2019-12-06 14:36:57 -0500

    Redireccion de las redes sociales

--- app/src/main/java/com/example/vitapet/FacebookLogin.java
+++ app/src/main/java/com/example/vitapet/FacebookLogin.java
@@ -19,7 +19,7 @@ public class FacebookLogin extends AppCompatActivity {
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent l=new Intent(FacebookLogin.this, User.class);
+                Intent l=new Intent(FacebookLogin.this, DashboardActivity.class);
                 startActivity(l);
             }
         });
