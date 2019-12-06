432c842e857c09da09c9a2555de9798e18e5c3a4
DenisLlc <dllamoca665@gmail.com>
2019-12-06 12:59:52 -0500

    dashboard activity menu

--- app/src/main/java/com/example/vitapet/DashboardActivity.java
+++ app/src/main/java/com/example/vitapet/DashboardActivity.java
@@ -5,25 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
-    TextView Usuario;
+    Button btn_News;
+    Button btn_Reports;
+    Button btn_Map;
+    Button btn_Pets;
+    Button btn_Perfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
-     Usuario = (TextView)findViewById(R.id.ds_lbl_user);
-     Usuario.setOnClickListener(new View.OnClickListener() {
-         @Override
-         public void onClick(View v) {
-             Intent u = new Intent(DashboardActivity.this,User.class);
-             startActivity(u);
-         }
-     });
+        //BTN NOTICIAS
+
+
+        //BTN REPORTES
+
+
+        //BTN MAPA
+
+
+        //BTN MASCOTAS
+
+
+        //BOTON PERFIL
+        btn_Perfil = (Button) findViewById(R.id.ds_btn_perfil);
+        btn_Perfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent p = new Intent(DashboardActivity.this, User.class);
+                startActivity(p);
+            }
+        });
 
     }
 }
